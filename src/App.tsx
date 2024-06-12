88f5750cb03e85acde46b4f69fb323f9c8cb880e
Brunhilde <brandontu35@gmail.com>
2024-06-12 17:17:56 -0400

    feat moved component stuff into main cause its a pain to move stuff between and it now sorta draws

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose";
 import "@tensorflow/tfjs-backend-webgl"
 import Webcam from 'react-webcam';
-import Canvas from './components/Canvas'
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+
+
 /*function Test() {
   const [res,setRes] = useState('');
   useEffect(() => {
@@ -23,53 +24,103 @@ import { useRef } from 'react';
 }*/
 
 function App() {
-  const webCamRef = useRef(null);
+  const webCamRef = useRef<Webcam>(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const ctxRef = useRef<CanvasRenderingContext2D | null>(null);
+  const [isPressed, setIsPressed] = useState(false);
 
   const loadHandPose = async () =>{
     const model = await handpose.load();
-    const predictions = await model.estimateHands(document.querySelector("video"));
-    if (predictions.length > 0) {
-      /*
-      `predictions` is an array of objects describing each detected hand, for example:
-      [
-        {
-          handInViewConfidence: 1, // The probability of a hand being present.
-          boundingBox: { // The bounding box surrounding the hand.
-            topLeft: [162.91, -17.42],
-            bottomRight: [548.56, 368.23],
-          },
-          landmarks: [ // The 3D coordinates of each hand landmark.
-            [472.52, 298.59, 0.00],
-            [412.80, 315.64, -6.18],
-            ...
-          ],
-          annotations: { // Semantic groupings of the `landmarks` coordinates.
-            thumb: [
-              [412.80, 315.64, -6.18]
-              [350.02, 298.38, -7.14],
-              ...
-            ],
-            ...
-          }
-        }
-      ]
-      */
-  
-      for (let i = 0; i < predictions.length; i++) {
-        const keypoints = predictions[i].landmarks;
-  
-        // Log hand keypoints.
-        for (let i = 0; i < keypoints.length; i++) {
-          const [x, y, z] = keypoints[i];
-          console.log(`Keypoint ${i}: [${x}, ${y}, ${z}]`);
-        }
+
+
+    const detect = async (model: handpose.HandPose) => {
+      if(typeof webCamRef.current !== "undefined" && webCamRef.current !== null && webCamRef.current.video?.readyState === 4) {
+        const videoFeed = webCamRef.current.video;
+        const vidWidth = webCamRef.current.video.videoWidth;
+        const vidHeight = webCamRef.current.video.videoHeight;
+
+        webCamRef.current.video.width = vidWidth;
+        webCamRef.current.video.height = vidHeight;
+
+        const predictions = await model.estimateHands(document.querySelector("video"));
+        console.log(predictions);
+        drawHand(predictions);
       }
     }
-    console.log("LOADED?");
+
+    setInterval(() =>{
+      detect(model)
+    },100)
   }
 
-  loadHandPose();
 
+  const startDrawing = (e: { nativeEvent: { offsetX: number; offsetY: number; }; }) => {
+    if (ctxRef.current == null) return;
+    ctxRef.current.beginPath();
+    ctxRef.current.moveTo(e.nativeEvent.offsetX,
+      e.nativeEvent.offsetY);
+    setIsPressed(true);
+    //console.log(e)
+  };
+
+  const endDrawing = () => {
+    ctxRef.current?.closePath();
+    setIsPressed(false);
+
+  };
+
+  const updateDrawing = (e: { nativeEvent: { offsetX: number; offsetY: number; }; }) => {
+    if (!isPressed) return;
+
+    ctxRef.current?.lineTo(e.nativeEvent.offsetX,
+      e.nativeEvent.offsetY)
+    ctxRef.current?.stroke();
+  };
+
+  const clear = () => {
+    if(ctxRef.current == null) return;
+    if(canvasRef.current == null) return;
+
+    ctxRef.current.clearRect(0,0,canvasRef.current.width,canvasRef.current.height);
+  }
+
+  const drawHand = (predictions: handpose.AnnotatedPrediction[]) => {
+    if(predictions.length > 0) {
+      predictions.forEach(prediction => {
+        const landmarks = prediction.landmarks;
+        for (let i = 0;i < landmarks.length;i++){
+          const x = landmarks[i][0];
+          const y = landmarks[i][0]
+          if (ctxRef.current == null) return;
+          ctxRef.current.beginPath();
+          ctxRef.current.arc(x,y,5,0,3* Math.PI);
+
+          ctxRef.current.fillStyle = "indigo";
+          ctxRef.current.fill()
+        }
+        
+      });
+    }
+  }
+
+
+
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (canvas == null) return;
+    canvas.width = 900 //im probably going to make it based on the window size
+    canvas.height = 800 //ditto
+
+    const ctx = canvas.getContext("2d");
+    if (ctx == null) return;
+    ctx.lineCap = "round";
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 5;
+    ctxRef.current = ctx
+  }, [])
+
+  loadHandPose();
 
   return (
     <>
@@ -79,7 +130,10 @@ function App() {
         width:400,
         height:400
       }}/>
-      <Canvas />
+      <div>
+      <button id="clearBtn" className='mt-3 mb-3' onClick={clear}>Clear</button>
+      <canvas ref={canvasRef} onMouseDown={startDrawing} onMouseMove={updateDrawing} onMouseUp={endDrawing} />
+    </div>
     </>
   )
 }
